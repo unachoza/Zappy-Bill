de177a924a1362bef733bfdddd2e24ad2636d505
arianna choza <unachoza@gmail.com>
2021-04-26 15:20:34 -0500

    saving current rate intput

--- zappy-bill/src/Components/Form/RateCheckInput/RateCheckInput.js
+++ zappy-bill/src/Components/Form/RateCheckInput/RateCheckInput.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './RateCheckInput.scss';
 
 const RateCheckInput = () => {
+  const [currentRate, setCurrentRate] = useState(null);
+
+  const onChangeValue = (event) => {
+    setCurrentRate(event.target.id);
+  };
   return (
-    <div className="form">
+    <div className="form" onClick={onChangeValue}>
       <div className="form__radio__group">
-        <input type="radio" className="form__radio__input" id="flat" name="currentRate" />
+        <input id="flat" type="radio" className="form__radio__input" name="currentRate" />
         <label for="flat" className="form__radio__label" id="flat">
           <span className="form__radio-button"></span>
           Flat Rate
