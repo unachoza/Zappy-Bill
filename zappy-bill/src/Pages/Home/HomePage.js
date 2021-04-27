978a7f0ea6a6c966d4394710907e15ce8d3cef80
arianna choza <unachoza@gmail.com>
2021-04-27 14:42:38 -0500

    basic calculations are accurate; need to account for edgecases

--- zappy-bill/src/Pages/Home/HomePage.js
+++ zappy-bill/src/Pages/Home/HomePage.js
@@ -2,7 +2,14 @@ import React, { useState } from 'react';
 import RateCheckInput from '../../Components/Form/RateCheckInput/RateCheckInput';
 import DrivingMilesInput from '../../Components/Form/DrivingMilesInput/DrivingMilesInput';
 import ChargeHoursInput from '../../Components/Form/ChargeHoursInput/ChargeHoursInput';
-import { INITIAL_USER_STATE, FLAT_RATE, INITIAL_HOME_LOAD, PEAK_HOURS_VALUES } from '../../Constants';
+import {
+  INITIAL_USER_STATE,
+  EV_KWH_RATE,
+  FLAT_RATE,
+  TOU_RATE,
+  INITIAL_HOME_LOAD,
+  PEAK_HOURS_VALUES,
+} from '../../Constants';
 import { useForm } from '../../Utility/useForm';
 import './HomePage.scss';
 
@@ -17,13 +24,39 @@ const HomePage = () => {
   const theIntersection = (arr1, arr2) => {
     return arr1.filter((elm) => arr2.includes(elm));
   };
+
   const calculateTOUHours = (chargingHours) => {
     const totalHours = chargingHours.endTime - chargingHours.startTime;
     const chargingRange = createHoursRange(totalHours, chargingHours.startTime);
     const totalPeakHours = theIntersection(PEAK_HOURS_VALUES, chargingRange).length;
     const peakChargePercentage = totalPeakHours / totalHours;
     const offPeakChargePercentage = (totalHours - totalPeakHours) / totalHours;
-    return peakChargePercentage, offPeakChargePercentage;
+    return { peakChargePercentage, offPeakChargePercentage };
+  };
+  const calculateOptimalRate = (currentRate, evBillImpactFlat, evBillImpactFlex) => {
+    let optimalChoice = {};
+    optimalChoice.price = Math.min(evBillImpactFlat, evBillImpactFlex);
+    console.log(Math.min(evBillImpactFlat, evBillImpactFlex));
+    optimalChoice.rate = currentRate === 'flat' ? `switch` : 'keep your rate';
+    optimalChoice.rate = `With Your Current ${currentRate} Rate, You Should ${optimalChoice.rate} To Be Cost Effective`;
+    return optimalChoice;
+  };
+  const calcElectricBill = (currentRate, milesDriven, chargingHours) => {
+    const evKWhConsumption = milesDriven * EV_KWH_RATE;
+    const evBillImpactFlat = evKWhConsumption * FLAT_RATE;
+    const usersChargingPercentages = calculateTOUHours(chargingHours);
+    const evBillImpactFlex =
+      evKWhConsumption * usersChargingPercentages.offPeakChargePercentage * TOU_RATE.peak +
+      evKWhConsumption * usersChargingPercentages.peakChargePercentage * TOU_RATE.offPeak;
+    const rateEvaluation = calculateOptimalRate(currentRate, evBillImpactFlat, evBillImpactFlex);
+    return rateEvaluation;
+  };
+  const handleSubmit = () => {
+    const { currentRate, milesDriven } = userData;
+    calculateTOUHours(chargingHours);
+    calcElectricBill(currentRate, milesDriven, chargingHours);
+    console.log(calcElectricBill(currentRate, milesDriven, chargingHours));
+    return true;
   };
 
   return (
